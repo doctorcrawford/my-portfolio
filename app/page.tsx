017a22c75016c23025304bf0a76e78d0406a9e4e
Kyle Crawford <kcraw217@gmail.com>
2023-09-11 16:03:09 -0700

    start

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-
+import {BsFillMoonStarsFill} from 'react-icons/bs';
 export default function Home() {
   return (
     <div>
       <main>
-
+        <section className="min-h-screen">
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1>developedbykyle</h1>
+            <ul>
+              <li>
+                <BsFillMoonStarsFill />
+              </li>
+              <li><a href="#">Resume</a></li>
+            </ul>
+          </nav>
+        </section>
       </main>
     </div>
   )
