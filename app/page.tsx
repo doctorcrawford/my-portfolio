0433aab1e23887132fcb06b1edfc5490131f8cde
Kyle Crawford <kcraw217@gmail.com>
2023-09-12 13:29:56 -0700

    add styling to header

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 export default function Home() {
   return (
     <div>
-      <main>
+      <main className='bg-white px-10'>
         <section className="min-h-screen">
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1>developedbykyle</h1>
-            <ul>
+            <h1 className='text-xl'>developedbykyle</h1>
+            <ul className='flex items-center'>
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
               </li>
-              <li><a href="#">Resume</a></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
             </ul>
           </nav>
         </section>
