7caac8b557efdda167a081b81e79a9a134592147
Kyle Crawford <kcraw217@gmail.com>
2023-09-12 15:56:53 -0700

    change project text

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-1'>Portfolio</h3>
             <p className='text-md py-2 leading-8 text-gray-800'>
-              Some projects I&apos;ve worked on.
+              Some projects I&apos;ve worked on. Check them out!
             </p>
             <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
               <div className='basis-1/3 flex-1'>
